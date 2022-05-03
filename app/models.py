0a57b5944602bc94dacb83cd84b427fcdc74115f
Brenda <bee@bee.com>
2022-05-04 00:43:13 +0300

    Adds the sources class

--- app/models.py
+++ app/models.py
@@ -29,3 +29,12 @@ class Headlines:
         self.time = time
         self.url = url
         self.image = image
+
+class Source:
+    '''Source class'''
+
+    def __init__(self,id,name,description,url):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
