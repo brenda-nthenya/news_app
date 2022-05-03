3de00c0ec6da1b23e79c5591bbc7e83aac1970ce
Brenda <bee@bee.com>
2022-05-04 00:42:11 +0300

    Adds the headlines class

--- app/models.py
+++ app/models.py
@@ -19,3 +19,13 @@ class Category:
         self.time = time
         self.url = url
         self.image = image
+
+class Headlines:
+
+    def __init__(self, author,title,description,time, url,image):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.time = time
+        self.url = url
+        self.image = image
